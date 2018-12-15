3fc09d4b1268e6a324991f1cc90beeb713796277
Michael Christian <michael.christian@traveloka.com>
2018-12-15 11:43:59 +0700

    Initial Commit

---
+++ EkoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EkoTest
+//
+//  Created by Michael Christian on 15/12/18.
+//  Copyright © 2018 Michael Christian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EkoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EkoTest
+//
+//  Created by Michael Christian on 15/12/18.
+//  Copyright © 2018 Michael Christian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
